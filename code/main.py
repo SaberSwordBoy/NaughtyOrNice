ee2e2a0782ad34b6b35e65ca7d2ce6327be0caba
Bryce <saberfilmsproductions@gmail.com>
2021-12-09 13:59:04 -0500

    Add better print statements to main.py

--- code/main.py
+++ code/main.py
@@ -1,21 +1,16 @@
 import pygame
 import random
 import time
-import pyttsx3 as tts
 import serial
 import os
 import subprocess
 import threading
 import string
 pygame.init()
-tts.init()
-
-engine = tts.Engine()
-engine.setProperty('rate', 125)
 
 serial = serial.Serial("/dev/ttyACM0", 9600, timeout=0.1)
 
-print("loaded, starting program")
+print("[!] Loading Sound files")
 naughtySounds = [
     pygame.mixer.Sound("voices/santa_hard_naughty.wav"),
     pygame.mixer.Sound("voices/santa_pile_of_coal.wav"),
@@ -30,18 +25,12 @@ niceSounds = [
         lenientSound,
 ]
 
-calculating = [
-    "Calculating you're naughty and nice score",
-    "Calculating naughty or nice levels...",
-    "Calculating whether you are naughty or nice..",
-    "calculating naughty and nice scores"
-]
-
 camerasounds1 = pygame.mixer.Sound("soundscrate-camera-sounds-various-1.wav")
 camerazoom = pygame.mixer.Sound("soundscrate-camera-zoom-2.wav")
 slotmachine = pygame.mixer.Sound("soundscrate-casino-slot-machine-2.wav")
 powerup2 = pygame.mixer.Sound("soundscrate-scifi-weapon-power-up-mid-2.wav")
 musicbox = pygame.mixer.Sound("soundscrate-christmas-music-box-1.wav")
+print("[!] Sound files loaded")
 
 def powerUpSequence():
     camerazoom.play()
@@ -65,23 +54,8 @@ def takePicture():
     output = subprocess.check_output(["fswebcam", f"images/{name}"])
     print(output)
 
-def sayText(text):
-    engine.say(text)
-    engine.runAndWait()
-
-def playResultSound(naughty):
-    print(naughty)
-    if naughty:
-        #sayText(random.choice(naughtySounds))
-        print("spoke")
-
-    else:
-        #sayText(random.choice(niceSounds))
-        print("spoke")
-
-
 def main():
-    print("here")
+    print("[!] Starting Main Loop")
     running = True
     button_pressed = False
     while running:
@@ -100,5 +74,6 @@ def main():
             powerUpSequence()
         if b"pressed" in serial_in:
             sayText(random.choice(calculating))
-print("starting....")
+            
+print("[!] Starting Raspi Code....")
 main()
